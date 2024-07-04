33f8093059cb6cf114fb8abe62a1e1b2a4d8586c
sebamiro <seba.mi.21@gmail.com>
2024-07-04 22:13:46 +0200

    add: Quiz data

--- database/db.go
+++ database/db.go
@@ -72,18 +72,128 @@ var (
 	}
 
 	GOLANG_QUIZ = []QuizQuestion{
-		{ID: 1, QuizID: 1, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 2, QuizID: 1, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 3, QuizID: 1, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 4, QuizID: 1, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 5, QuizID: 1, Question: "", Answers: []string{}, CorrectAnswer: 0},
+		{
+			ID:       1,
+			QuizID:   1,
+			Question: "What keyword is used to define a package in Go?",
+			Answers: []string{
+				"namespace",
+				"package",
+				"module",
+				"library",
+			},
+			CorrectAnswer: 1,
+		},
+		{
+			ID:       2,
+			QuizID:   1,
+			Question: "How do you declare a variable in Go?",
+			Answers: []string{
+				"var x int",
+				"int x",
+				"declare x int",
+				"define x int",
+			},
+			CorrectAnswer: 0,
+		},
+		{
+			ID:       3,
+			QuizID:   1,
+			Question: "What is the correct way to create a slice in Go?",
+			Answers: []string{
+				"var s []int",
+				"var s int[]",
+				"var int[] s",
+				"s := []int",
+			},
+			CorrectAnswer: 0,
+		},
+		{
+			ID:       4,
+			QuizID:   1,
+			Question: "Which of the following is the correct way to handle errors in Go?",
+			Answers: []string{
+				"try { ... } catch { ... }",
+				"expect { ... }",
+				"if err != nil { ... }",
+				"error { ... }",
+			},
+			CorrectAnswer: 2,
+		},
+		{
+			ID:       5,
+			QuizID:   1,
+			Question: "What is the name of the tool used for formatting Go code?",
+			Answers: []string{
+				"goformat",
+				"go-fmt",
+				"formatgo",
+				"gofmt",
+			},
+			CorrectAnswer: 3,
+		},
 	}
 
 	BARCELONA_QUIZ = []QuizQuestion{
-		{ID: 1, QuizID: 2, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 2, QuizID: 2, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 3, QuizID: 2, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 4, QuizID: 2, Question: "", Answers: []string{}, CorrectAnswer: 0},
-		{ID: 5, QuizID: 2, Question: "", Answers: []string{}, CorrectAnswer: 0},
+		{
+			ID:       1,
+			QuizID:   2,
+			Question: "What is the name of the famous basilica in Barcelona designed by Antoni Gaudí?",
+			Answers: []string{
+				"La Sagrada Familia",
+				"La Catedral",
+				"La Almudena",
+				"La Giralda",
+			},
+			CorrectAnswer: 0,
+		},
+		{
+			ID:       2,
+			QuizID:   2,
+			Question: "Which of the following is a famous street in Barcelona known for its shops and cafes?",
+			Answers: []string{
+				"Champs-Élysées",
+				"Oxford Street",
+				"Fifth Avenue",
+				"Las Ramblas",
+			},
+			CorrectAnswer: 3,
+		},
+		{
+			ID:       3,
+			QuizID:   2,
+			Question: "Which park in Barcelona is known for its mosaics and architecture by Gaudí?",
+			Answers: []string{
+				"Retiro Park",
+				"Hyde Park",
+				"Park Güell",
+				"Central Park",
+			},
+			CorrectAnswer: 2,
+		},
+		{
+			ID:       4,
+			QuizID:   2,
+			Question: "What is the name of Barcelona's main football stadium?",
+			Answers: []string{
+				"Santiago Bernabéu Stadium",
+				"Camp Nou",
+				"San Siuro",
+				"Allianz Arena",
+			},
+			CorrectAnswer: 1,
+		},
+		{
+			ID:       5,
+			QuizID:   1,
+			Question: "Which historic neighborhood in Barcelona is known for its Gothic architecture and narrow streets?",
+			Answers: []string{
+				"El Raval",
+				"Gràcia",
+				"El Born",
+				"gofmt",
+			},
+			CorrectAnswer: 3,
+		},
 	}
 )
