d8e3307d8f4db716360f1fb27c00e3b3e977b860
sebamiro <seba.mi.21@gmail.com>
2024-07-04 22:13:57 +0200

    add: Building routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/sebamiro/go-quiz/routes"
 	"github.com/sebamiro/go-quiz/services"
 )
 
 func main() {
 	c := services.NewContainer()
+
+	routes.BuildRoutes(c)
+
 	c.Web.Start(":3000")
 }
