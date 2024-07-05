e51f81e41522af83db9c7d443d1fd093c6129018
sebamiro <seba.mi.21@gmail.com>
2024-07-05 18:38:15 +0200

    add: Print interface

--- database/schema.go
+++ database/schema.go
@@ -1,5 +1,10 @@
 package database
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Quiz struct {
 	ID    uint   `json:"id"`
 	Title string `json:"title"`
@@ -11,6 +16,15 @@ type QuizQuestion struct {
 	Question      string   `json:"question"`
 	Answers       []string `json:"answers"`
 	CorrectAnswer uint
+}
+
+func (q QuizQuestion) String() string {
+	lines := make([]string, 0)
+	lines = append(lines, fmt.Sprintf("- %s\n", q.Question))
+	for i, a := range q.Answers {
+		lines = append(lines, fmt.Sprintf("\t%d - %s\n", i, a))
+	}
+	return strings.Join(lines, "")
 }
 
 type QuizResponse struct {
