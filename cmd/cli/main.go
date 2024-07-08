718d8441fd78bc83ef5e0cbb24e57242a632d650
sebamiro <seba.mi.21@gmail.com>
2024-07-08 15:48:17 +0200

    update: App name

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"os"
-
 	"github.com/sebamiro/go-quiz/pkg/commands"
 	"github.com/spf13/cobra"
 )
 
+const APP_NAME = "quiz-cli"
+
 func main() {
-	var rootCmd = &cobra.Command{Use: os.Args[0]}
+	var rootCmd = &cobra.Command{Use: APP_NAME}
 
 	commands.BuildCommands(rootCmd)
 	rootCmd.Execute()
